ef532e62e242bb498feb35ed451a0fda5fb7ae8f
Mohamed ElHalfawy <mohamed.halfawyy@gmail.com>
2022-12-19 03:03:51 +0200

    BUGFIX/fix logo icon & progress bar animation and fonts & fixed issues in about me page

--- js/script.js
+++ js/script.js
@@ -102,6 +102,80 @@ window.addEventListener("load", () => {
     setTimeout(() => {
         loader.style.display = "none";
         mainContent.classList.remove("hidden");
-        }, 2000)
+    }, 2500)
 
 })
+/*   progress bar animation   */
+
+const java_progressBar = document.querySelector(".java-bar span");
+const python_progressBar = document.querySelector(".python-bar span");
+const js_progressBar = document.querySelector(".js-bar span");
+const react_progressBar = document.querySelector(".react-bar span");
+const angular_progressBar = document.querySelector(".angular-bar span");
+const flutter_progressBar = document.querySelector(".flutter-bar span");
+const html_progressBar = document.querySelector(".html-bar span");
+const css_progressBar = document.querySelector(".css-bar span");
+const sql_progressBar = document.querySelector(".sql-bar span");
+const firebase_progressBar = document.querySelector(".firebase-bar span");
+const sql_server_progressBar = document.querySelector(".sql-server-bar span");
+const mongoDB_progressBar = document.querySelector(".mongo-bar span");
+const rest_progressBar = document.querySelector(".rest-bar span");
+const graph_progressBar = document.querySelector(".graph-bar span");
+const git_progressBar = document.querySelector(".git-bar span");
+const aws_progressBar = document.querySelector(".aws-bar span");
+const docker_progressBar = document.querySelector(".docker-bar span");
+const oop_progressBar = document.querySelector(".oop-bar span");
+const data_progressBar = document.querySelector(".data-bar span");
+
+const observer = new IntersectionObserver(entries => {
+    // Loop over the entries
+    entries.forEach(entry => {
+        // If the element is visible
+        if (entry.isIntersecting) {
+            // Add the animation class
+            setTimeout(() => {
+                react_progressBar.classList.add('react');
+                java_progressBar.classList.add('java');
+                python_progressBar.classList.add('python');
+                js_progressBar.classList.add('js');
+                angular_progressBar.classList.add('angular');
+                flutter_progressBar.classList.add('flutter');
+                html_progressBar.classList.add('html');
+                css_progressBar.classList.add('css');
+            },1000);
+
+        }
+    });
+});
+const observer2 = new IntersectionObserver(entries => {
+    // Loop over the entries
+    entries.forEach(entry => {
+        // If the element is visible
+        if (entry.isIntersecting) {
+            // Add the animation class
+            sql_progressBar.classList.add('sql');
+            firebase_progressBar.classList.add('firebase');
+            sql_server_progressBar.classList.add('sql-server');
+            mongoDB_progressBar.classList.add('mongoDB');
+            rest_progressBar.classList.add('restAPI');
+            graph_progressBar.classList.add('graphQL');
+            git_progressBar.classList.add('git');
+            aws_progressBar.classList.add('aws');
+        }
+    });
+});
+const observer3 = new IntersectionObserver(entries => {
+    // Loop over the entries
+    entries.forEach(entry => {
+        // If the element is visible
+        if (entry.isIntersecting) {
+            // Add the animation class
+            docker_progressBar.classList.add('docker');
+            oop_progressBar.classList.add('oop');
+            data_progressBar.classList.add('data');
+        }
+    });
+});
+observer.observe(document.querySelector('.java-bar'));
+observer2.observe(document.querySelector('.sql-bar'));
+observer3.observe(document.querySelector('.docker-bar'));
